ddd7b47418e0a5a7d8318b8d647ef7b0bd68a752
Dora Telekesi <dorothea0929@gmail.com>
2025-10-08 18:18:42 +0200

    Add basic backend security

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,44 @@ import ExpressError from "./utils/ExpressError.js";
 
 import sparePartRoutes from "./routes/spare-part.js";
 import userRoutes from "./routes/user.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import sanitizeV5 from "./utils/mongoSanitizeV5.js";
 
 const app = express();
+
 
 //depends what type of body parsing I want, for regular form -->
 app.use(express.urlencoded({ extended: true }));
 //for json - raw data -->
 app.use(express.json());
+
+app.use(sanitizeV5({ replaceWith: "_" }));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'"],
+      scriptSrc: ["'unsafe-inline'", "'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      workerSrc: ["'self'", "blob:"],
+      childSrc: ["blob:"],
+      objectSrc: [],
+      imgSrc: ["'self'", "data:"],
+      fontSrc: ["'self'"],
+    },
+  })
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100, 
+  standardHeaders: true, 
+  legacyHeaders: false, 
+});
+
+app.use(limiter);
+
 
 app.use("/", userRoutes);
 app.use("/spare-part-list", sparePartRoutes);
