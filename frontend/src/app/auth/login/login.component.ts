31eea17fd679ff0858697cee475d2ad4137fccb4
Dora Telekesi <dorothea0929@gmail.com>
2025-10-16 20:15:56 +0200

    add login page design with reactive form

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private formBuilder: FormBuilder) {}
+  loginForm = this.formBuilder.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['', Validators.required]
+  });
 
+  onSubmit() {}
 }
